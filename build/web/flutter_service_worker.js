14c334d85f3f27c82886c66df000a76e94baf22e
Igor Passos <igormpassos12@gmail.com>
2025-08-19 23:31:10 -0300

    build: Adicionar build web compilado

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "288280f3679671a9cf50e22f0a4ee23f",
+const RESOURCES = {"flutter_bootstrap.js": "caa9b7e8948e2f054210f683f0af7ad2",
 "version.json": "bd37373882f8e47930f4b2b21f1c4571",
 "splash/img/light-2x.png": "69f80339dd3fa5d5d5e850e3ab307323",
 "splash/img/dark-4x.png": "f9c79095a5d7a39632c75ec4e9dbeb13",
